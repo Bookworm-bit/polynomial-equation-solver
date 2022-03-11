ad07f5103f4c511e1fd7b071ea0a2c1c780d0c98
Bookworm-bit <81590955+Bookworm-bit@users.noreply.github.com>
2022-03-11 14:45:52 -0500

    i guess the dictionary stuff works now

--- main.py
+++ main.py
@@ -9,24 +9,24 @@ def listMake(listy):
         if item[0] == 'x':
             diction[str(listy.index(item)) + 'coefficient'] = "1"
         if item[0] != 'x' and item not in operator:
-            partit = [item.partition("x")]
+            partit = item.partition("x")
             diction[str(listy.index(item)) + 'coefficient'] = str(partit[0])
         if "x" not in item and item not in operator:
             diction[str(listy.index(item)) + 'coefficient'] = f'C: {item}'
         if item in operator:
-            diction[str(listy.index(item)) + 'power'] = item
-        if diction[str(listy.index(item)) + 'coefficient'] != "1":
-            if "^" in item:
-                diction[str(listy.index(item)) + 'power'] = item.removeprefix(str(diction[str(listy.index(item)) + 'coefficient']) + 'x^')
-        if diction[str(listy.index(item)) + 'coefficient'] == "1":
-            if "^" in item:
-                diction[str(listy.index(item)) + 'power'] = item.removeprefix('x^')
-        if "x" in item and "^" not in item:
-            diction[str(listy.index(item)) + 'power'] = "1"
-        if "x" not in item and "^" not in item:
-            diction[str(listy.index(item)) + 'power'] = "0"
+            diction[str(listy.index(item)) + 'operator'] = item
+        if item not in operator:
+            if diction[str(listy.index(item)) + 'coefficient'] != "1":
+                if "^" in item:
+                    diction[str(listy.index(item)) + 'power'] = item.removeprefix(str(diction[str(listy.index(item)) + 'coefficient']) + 'x^')
+            if diction[str(listy.index(item)) + 'coefficient'] == "1":
+                if "^" in item:
+                    diction[str(listy.index(item)) + 'power'] = item.removeprefix('x^')
+            if "x" in item and "^" not in item:
+                diction[str(listy.index(item)) + 'power'] = "1"
+            if "x" not in item and "^" not in item:
+                diction[str(listy.index(item)) + 'power'] = "0"
     return diction
-    
 
 print("Welcome to Polynomial Equation Solver by:")
 print("""▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
