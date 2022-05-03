3e28145885b14126cbbf3fc3bc9f766d79f19546
Bookworm-bit <81590955+Bookworm-bit@users.noreply.github.com>
2022-05-03 09:54:55 -0400

    Conforming to PEP and general cleanup

--- main.py
+++ main.py
@@ -1,85 +1,110 @@
 import math
 import cmath
 
-operator = ['+', '-'] # accepted operators
-coefficients = []
-operators = []
-powers = []
-const = 0
+operator_list = ['+', '-']
+polynomial_coefficients = []
+polynomial_constant_factors = []
+leading_coefficient_factors = []
+polynomial_operators = []
+polynomial_exponents = []
+possible_roots_reciprocal = []
+unfllipped_possible_roots = []
+polynomial_constant= 0
 
-def listMake(listy):
+
+def parse(listy):
     for item in listy:
         if item[0] == 'x':
-            coefficients.append(1)
-        if item[0] != 'x' and item not in operator:
+            polynomial_coefficients.append(1)
+
+        if item[0] != 'x' and item not in operator_list:
             partit = item.partition("x")
-            coefficients.append(int(partit[0]))
-        if "x" not in item and item not in operator:
-            const += int(item)
-        if item in operator:
-            operators.append(int(item))
-        if item not in operator:
-            powers.append(int(item.partition("x^")[1]))
+            polynomial_coefficients.append(int(partit[0]))
+
+        if "x" not in item and item not in operator_list:
+            polynomial_constant+= int(item)
+
+        if item in operator_list:
+            polynomial_operators.append(int(item))
+
+        if item not in operator_list:
+            polynomial_exponents.append(int(item.partition("x^")[1]))
             if "x" in item and "^" not in item:
-                powers.append(1)
+                polynomial_exponents.append(1)
+
             if "x" not in item and "^" not in item:
-                powers.append(0)
-    return coefficients, operators, powers, const
+                polynomial_exponents.append(0)
+        
+        if len(coeff) - len(polynomial_operators) == 2:
+            polynomial_operators.insert(0, '+')
+
+    return polynomial_coefficients, polynomial_operators, polynomial_exponents, const
+
 
 og = input("Enter your polynomial equation: ")
 listy = og.split(' ')
 
-def factorFind(coefficients, powers):
-    global constantFac
-    global leadcoeffFac
-    constantFac = []
-    leadcoeffFac = []
+
+def factoring(polynomial_coefficients, polynomial_exponents):
+    global polynomial_constant_factors
+    global leading_coefficient_factors
+    polynomial_constant_factors = []
+    leading_coefficient_factors = []
     coeff = []
+
     for i in range(len(listy) - listy.count("+") - listy.count("-") + 1):
         if i % 2 == 0:
             coeff.append(i)
-        for x in range(0 - coefficients[-1], coefficients[-1] + 1):
+
+        for x in range(0 - polynomial_coefficients[-1], polynomial_coefficients[-1] + 1):
             if x != 0:
-                if coefficients[-1] % x == 0:
-                    constantFac.append(x)
-    for i in range(0 - coefficients[0], coefficients[0] + 1):
-        leadcoeffFac.append(i)
+                if polynomial_coefficients[-1] % x == 0:
+                    polynomial_constant_factors.append(x)
+
+    for i in range(0 - polynomial_coefficients[0], polynomial_coefficients[0] + 1):
+        leading_coefficient_factors.append(i)
+
     try:
-        leadcoeffFac.remove(None)
-        constantFac.remove(None)
+        leading_coefficient_factors.remove(None)
+        polynomial_constant_factors.remove(None)
     except ValueError:
         pass
+
     # try:
     #     if int(diction['0coefficient']) > 0:
-    #         for item in leadcoeffFac:
+    #         for item in leading_coefficient_factors:
     #             if int(item) < 0:
-    #                 leadcoeffFac.remove(item)
+    #                 leading_coefficient_factors.remove(item)
     #     for item in diction.keys():
     #         if int(item) < 0 and 'C:' in item:
-    #             for item in constantFac:
+    #             for item in polynomial_constant_factors:
     #                 if int(item) > 0:
-    #                     constantFac.remove(item)
+    #                     polynomial_constant_factors.remove(item)
     # except ValueError:
     #     pass
-    return leadcoeffFac, constantFac
 
-def combine(leadcoeffFac, constantFac):
-    global comb
-    comb = [f'{x}/{y}' for x in leadcoeffFac for y in constantFac]
-    return comb
-
-def recip(leadcoeffFac, constantFac):
-    global recip
-    recip = [f'{y}/{x}' for y in constantFac for x in leadcoeffFac]
-    return recip
-
-poss = comb + recip
-
-#def test(poss):
+    return leading_coefficient_factors, polynomial_constant_factors
 
 
-# def synthetic()
+def unfllippedroots(leading_coefficient_factors, polynomial_constant_factors):
+    global unfllipped_possible_roots
+    unfllipped_possible_roots = [f'{x}/{y}' for x in leading_coefficient_factors for y in polynomial_constant_factors]
+    return unfllipped_possible_roots
 
+
+def reciprocalroots(leading_coefficient_factors, polynomial_constant_factors):
+    global possible_roots_reciprocal
+    possible_roots_reciprocal = [f'{y}/{x}' for y in polynomial_constant_factors for x in leading_coefficient_factors]
+    return possible_roots_reciprocal
+
+
+possible_polynomial_roots = unfllipped_possible_roots + possible_roots_reciprocal
+
+
+# def plugin(poss):
+
+
+# def syntheticdivision()
 
 
 print("Welcome to Polynomial Equation Solver by:")
@@ -90,7 +115,7 @@ print("""
 ██░▀▀░██▄▄███▄▄██▄█▄██▄█▄███▄▄██▄█▄▄█▄███▄████▄▄▄▄█▄▄▄██▄███
 ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
 """)
-print(listMake(listy))
-print(factorFind(coefficients))
-print(combine(leadcoeffFac, constantFac))
-print(recip(leadcoeffFac, constantFac))
+print(parse(listy))
+print(factoring(polynomial_coefficients))
+print(unfllippedroots(leading_coefficient_factors, polynomial_constant_factors))
+print(reciprocalroots(leading_coefficient_factors, polynomial_constant_factors))
